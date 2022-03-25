fac9b104fd6c7b1e0f954fc0e2b9dfd76a8d7958
Aya Elawdan <32736651+virtlabx@users.noreply.github.com>
2022-03-26 00:51:32 +0100

    Added target group, load balancer listener for vault and hardening the sg (#20)
    * Added target group, load balancer listener for vault
    
    * Fixing typo
    
    * Fixed the lb-vault-tg wrong port number
    
    * Harden the sg
    
    * Added a small hint in readme file
    
    Co-authored-by: Aya Elawdan <Aya.Elawdan@ingenico.com>

--- loadbalancers.tf
+++ loadbalancers.tf
@@ -7,7 +7,7 @@ resource "aws_lb" "jenkins-vault-lb" {
   tags               = merge({ Name = "lb-jenkins-vault" }, local.dev-tags)
 }
 
-resource "aws_lb_listener" "HTTPS" {
+resource "aws_lb_listener" "https_jenkins" {
   load_balancer_arn = aws_lb.jenkins-vault-lb.arn
   port              = "443"
   protocol          = "HTTPS"
@@ -15,29 +15,16 @@ resource "aws_lb_listener" "HTTPS" {
   certificate_arn   = aws_acm_certificate.jenkins_vault_bloomreach_certificate.arn
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.lb-jenkins-vault-tg.arn
+    target_group_arn = aws_lb_target_group.lb-jenkins-tg.arn
   }
 }
 
-resource "aws_lb_listener_rule" "https-listener-rule" {
-  listener_arn = aws_lb_listener.HTTPS.arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lb-jenkins-vault-tg.arn
-  }
-  condition {
-    host_header {
-      values = ["jenkins-vault-bloomreach.com"]
-    }
-  }
-}
-
-resource "aws_lb_target_group" "lb-jenkins-vault-tg" {
-  name     = "lb-jenkins-vault-tg"
+resource "aws_lb_target_group" "lb-jenkins-tg" {
+  name     = "lb-jenkins-tg"
   port     = "8080"
   protocol = "HTTP"
   vpc_id   = module.eks-vpc.vpc_id
-  tags     = merge({ Name = "lb-jenkins-vault-tg" }, local.dev-tags)
+  tags     = merge({ Name = "lb-jenkins-tg" }, local.dev-tags)
   health_check {
     protocol            = "HTTP"
     path                = "/login"
@@ -50,8 +37,44 @@ resource "aws_lb_target_group" "lb-jenkins-vault-tg" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "lb-jenkins-vault-tg-attachment" {
-  target_group_arn = aws_lb_target_group.lb-jenkins-vault-tg.arn
+resource "aws_lb_target_group_attachment" "lb-jenkins-tg-attachment" {
+  target_group_arn = aws_lb_target_group.lb-jenkins-tg.arn
   target_id        = flatten(module.jenkins-dev-master.instance_id)[0]
   port             = "8080"
+}
+
+resource "aws_lb_listener" "https_vault" {
+  load_balancer_arn = aws_lb.jenkins-vault-lb.arn
+  port              = "8200"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.jenkins_vault_bloomreach_certificate.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb-vault-tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "lb-vault-tg" {
+  name     = "lb-vault-tg"
+  port     = "8200"
+  protocol = "HTTP"
+  vpc_id   = module.eks-vpc.vpc_id
+  tags     = merge({ Name = "lb-vault-tg" }, local.dev-tags)
+  health_check {
+    protocol            = "HTTP"
+    path                = "/ui"
+    port                = "8200"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "307"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "lb-vault-tg-attachment" {
+  target_group_arn = aws_lb_target_group.lb-vault-tg.arn
+  target_id        = flatten(module.jenkins-dev-master.instance_id)[0]
+  port             = "8200"
 }
